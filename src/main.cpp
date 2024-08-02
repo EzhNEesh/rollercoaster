00ec24807cee06b6aed3090b1accc4ea94f480dd
EzhNEEsh <oyaiskander@yandex.ru>
2024-08-02 12:20:20 +0300

    feat: using namespace from main removed and checking counts of arguments added

--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,25 @@
 #include "normaldistribution.h"
 #include "rollercoaster.h"
 
-using namespace std;
-
 int main(int argc, char *argv[])
 {
+    if (argc != 5) {
+        std::cout << "Count of arguments must equal 5" << std::endl;
+    }
+
     BinaryReader binReader;
-    vector<float> nums = binReader.readBinary(argv[1]);
+    std::vector<float> nums = binReader.readBinary(argv[1]);
 
     NormalDistribution nDistribution{nums};
     int noizeFloor = nDistribution.getBorders().second;
 
     RollerCoaster rollerCoaster{nums};
-    vector<pair<unsigned int, unsigned int>> results = rollerCoaster.findRollerCoasters(atoi(argv[3]), atoi(argv[4]), noizeFloor);
+    std::vector<std::pair<unsigned int, unsigned int>> results = rollerCoaster.findRollerCoasters(atoi(argv[3]), atoi(argv[4]), noizeFloor);
 
-    ofstream file(argv[2]);
+    std::ofstream file(argv[2]);
     if (file.is_open()) {
         for (unsigned int i = 0; i < results.size(); i++) {
-            file << results[i].first << ' ' << results[i].second << endl;
+            file << results[i].first << ' ' << results[i].second << std::endl;
         }
     }
     return 0;
